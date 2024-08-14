a34f31d837766f4e42694753cda42e011b432166
Edward <edward940715@gmail.com>
2024-08-14 16:30:49 +0800

    Edward測試fork

--- root/index.php
+++ root/index.php
@@ -3,23 +3,25 @@ include("../pdoConnect.php");
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
 
-        <?php include("../js.php") ?>
-    </head>
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <body>
-        <div class="container">
-            <h1>user change test</h1>
-        </div>
-        <?php include("../nav.php"); ?>
-        <?php include("../footer.php"); ?>
-    </body>
+    <?php include("../js.php") ?>
+</head>
+
+<body>
+    <div class="container">
+        <h1>user change test</h1>
+        <span>Edward forktest</span>
+    </div>
+    <?php include("../nav.php"); ?>
+    <?php include("../footer.php"); ?>
+</body>
+
 </html>
