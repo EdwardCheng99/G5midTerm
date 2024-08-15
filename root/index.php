4455e2d40829b8cf09fb8fddf4087801c8534cde
Bruce <paydayucky@gmail.com>
2024-08-15 15:28:27 +0800

    pdoconnect測試

--- root/index.php
+++ root/index.php
@@ -1,5 +1,26 @@
 <?php
 include("../pdoConnect.php");
+
+// sql connect test
+$sql = "SELECT * FROM Member WHERE MemberValid = 1 AND MemberID = 1";
+
+$stmt = $dbHost->prepare($sql);
+
+try {
+    $stmt->execute();
+	while ($row = $stmt->fetch()) {
+    	// echo "接收到的資料：<pre>";
+    	// print_r($row);
+    	// echo "</pre>";
+	}
+    
+} catch (PDOException $e) {
+    echo "預處理陳述式執行失敗！ <br/>";
+    echo "Error: " . $e->getMessage() . "<br/>";
+    $dbHost = NULL;
+    exit;
+}
+print_r($row);
 ?>
 <!doctype html>
 <html lang="en">
@@ -17,7 +38,14 @@ include("../pdoConnect.php");
 
 <body>
     <div class="container">
-        
+        <h1>Member</h1>
+        <?php 
+        $stmt->execute();
+        while ($row = $stmt->fetch()) {
+            echo "接收到的資料：<pre>";
+            print_r($row);
+            echo "</pre>";
+        } ?>
     </div>
     <?php include("../nav.php"); ?>
     <?php include("../footer.php"); ?>
