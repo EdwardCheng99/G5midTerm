de5e582a4b66b038e02445bf30eaa02de5096f6a
Edward <edward940715@gmail.com>
2024-08-25 23:20:58 +0800

    註解footer

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-<footer>
+<!-- <footer>
     <div class="footer clearfix mb-0 text-muted">
-        <div class="float-center">
-            <p>2024 &copy; MFEE57G5</p>
+        <div class="float-start">
+            <p>2023 &copy; Mazer</p>
         </div>
         <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by <a href="https://saugi.me">Saugi</a></p>
         </div>
     </div>
-</footer>
+</footer> -->
